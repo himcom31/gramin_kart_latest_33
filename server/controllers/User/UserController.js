306c9fcac4948193efc71a281e9a67f9a26c843a
Himanshurchaudhary <rchaudhary9771@gmail.com>
2026-08-22 23:24:02 +0530

    Initial commit467

--- server/controllers/User/UserController.js
+++ server/controllers/User/UserController.js
@@ -8,7 +8,7 @@ const generateToken = (userId) => {
     return jwt.sign(
         { id: userId },
         process.env.JWT_SECRET,
-        { expiresIn: process.env.JWT_EXPIRES_IN || '7d' }
+        { expiresIn: process.env.JWT_EXPIRES_IN || '365d' }
     );
 };
 
